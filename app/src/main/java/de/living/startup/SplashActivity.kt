bb394cd2377e96225c1c11cb2c6cfd298b107053
Sascha Bichler <sascha.bichler@googlemail.com>
2022-06-25 15:30:55 +0200

    changed to Viewbinding

--- app/src/main/java/de/living/startup/SplashActivity.kt
+++ app/src/main/java/de/living/startup/SplashActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import de.living.MainActivity
